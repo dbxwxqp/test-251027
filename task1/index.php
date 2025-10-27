090f707a3feeebac3489b75bc2cb02c7f15dc644
t.guz4 <dbxwxqp@gmail.com>
2025-10-27 11:46:09 +0300

    Task #1

--- task1/index.php
+++ task1/index.php
@@ -1,5 +1,41 @@
 <?php
+
+?>
+
+    <h1>Задача</h1>
+
+    <h2>Прочитать файл mock_data.json (он находится в этой же папке).</h2>
+<?php
 $file_path = __DIR__ . '/mock_data.json';
 $file_data = file_get_contents($file_path);
 $aData = json_decode($file_data, true);
-//die('#<pre>' . print_r($file_data, true) . '</pre>');
+print('<pre>' . print_r($aData, true) . '</pre>');
+?>
+    <hr>
+
+    <h2>Вывести всех пользователей в удобном формате.</h2>
+<?php
+foreach($aData['users'] as $user){
+    echo <<<HD
+<div class="users" title="User #{$user['ID']}">
+    <a href="mailto:{$user['EMAIL']}">{$user['NAME']}</a>
+</div>
+        
+HD;
+}
+?>
+    <hr>
+
+    <h2>Вывести все сделки со статусом <span style="opacity: .1">WON или</span> LOSE.</h2>
+<?php
+foreach($aData['deals'] as $deal) {
+    if($deal['STATUS'] != 'LOSE') continue;
+
+    echo <<<HD
+<div class="deals" title="Deal #{$deal['ID']}">
+    <strong>{$deal['TITLE']}</strong> / <span>{$deal['AMOUNT']}</span>
+</div>
+        
+HD;
+}
+
